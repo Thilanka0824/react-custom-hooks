4e50337bcb53ccf7f6979c0af630432ac15e8ad2
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-03-21 12:44:20 -0700

    luis

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import useStringHook from './hooks/StringHook';
+
+function App() {
+
+  
+
+  return (
+    <div className="App App-header">
+  <h1>Custom String Hooks</h1>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/StringHook.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+const useStringHook = () => {
+  
+  
+    return (
+    <div>StringHook</div>
+  )
+}
+
+export default useStringHook
